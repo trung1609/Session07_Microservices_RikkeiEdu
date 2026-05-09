e05fd3d012f08eb098dd05ae534b9b80739be5f5
trung1609 <vmt162005@gmail.com>
2026-05-09 21:05:56 +0700

    Add WelcomeController with dynamic message from config
    Introduce a new WelcomeController to serve a dynamic welcome message retrieved from the configuration server. Enabled the /refresh endpoint to allow configuration updates at runtime.

---
+++ patient-service/src/main/java/com/trung/patientservice/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.trung.patientservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+@RefreshScope
+public class WelcomeController {
+    @Value("${app.welcome}")
+    private String welcome;
+
+    @GetMapping
+    public String welcome() {
+        return welcome;
+    }
+}
